1934d63ef55a0c1654f8285f3d875a38be5f0d39
andrewhamilton <andrew.hamilton@exelisinc.com>
2016-02-26 21:30:03 -0500

    can now delete grocery items

--- js/client.js
+++ js/client.js
@@ -1,18 +1,32 @@
 $(function () {
-  var server = io.connect(window.location.href);
-  server.on('connect', function(){
-  });
-  server.on('new grocery item', function(new_grocery_item){
-    $('#list_items').show().append('<li>' + new_grocery_item + '</li>');
-    $('#no_items').hide();
-  });
-  $('#new_item_form').submit(function(e){
-    e.preventDefault();
-    var $new_item_input = $('#new_item_input');
-    var new_item = $new_item_input.val();
-    if (new_item.length > 0) {
-      server.emit('new grocery item', $new_item_input.val());
-      $new_item_input.val('');
-    }
-  });
+    var server = io.connect(window.location.href);
+    server.on('connect', function () {
+    });
+    server.on('new grocery item', function (new_grocery_item) {
+        console.log("received item: "+ new_grocery_item);
+        var grocery_item = JSON.parse(new_grocery_item);
+        var $li = $('<li><span class="grocery-item">' + grocery_item.item + '</span></li>');
+        var $deleteButton = $('<span data-index="' + grocery_item.index + '" class="delete-button">&times</span>');
+        $li.append($deleteButton);
+        $('#grocery-items').show().append($li);
+        $deleteButton.click(function(){
+            console.log('delete');
+            var index = parseInt($(this).attr("data-index"));
+            server.emit("remove grocery item", index);
+        });
+        $('#no_items').hide();
+    });
+    server.on('remove grocery item', function(index){
+        console.log("received remove request for item: " + index);
+        $('#grocery-items').find("[data-index='" + index + "']").parent().remove();
+    });
+    $('#new_item_form').submit(function (e) {
+        e.preventDefault();
+        var $new_item_input = $('#new_item_input');
+        var new_item = $new_item_input.val();
+        if (new_item.length > 0) {
+            server.emit('new grocery item', $new_item_input.val());
+            $new_item_input.val('');
+        }
+    });
 });
--- server.js
+++ server.js
@@ -3,27 +3,47 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-var broadcastNewItem = function(client, new_grocery_item){
-  client.broadcast.emit('new grocery item', new_grocery_item);
-  client.emit('new grocery item', new_grocery_item);
+var groceryItems = [];
+var index = 0;
+
+var broadcastAll = function (client, message, data) {
+    console.log("broadcasting: " + data);
+    client.broadcast.emit(message, data);
+    client.emit(message, data);
 };
 
-io.on('connection', function(client){
-  client.on('new grocery item', function(new_grocery_item){
-    broadcastNewItem(client, new_grocery_item);
-  });
+io.on('connection', function (client) {
+    groceryItems.forEach(function (grocery_item) {
+        client.emit('new grocery item', JSON.stringify(grocery_item));
+    });
+    client.on('new grocery item', function (new_grocery_item) {
+        console.log('received item: ' + new_grocery_item);
+        var grocery_item = {index: index++, item: new_grocery_item};
+        groceryItems.push(grocery_item);
+        broadcastAll(client, "new grocery item", JSON.stringify(grocery_item));
+    });
+    client.on('remove grocery item', function(index){
+        console.log("received remove request for item: " + index);
+        for (i=0; i<groceryItems.length; i++){
+            if (groceryItems[i].index === index){
+                groceryItems.splice(i, 1);
+                break;
+            }
+        }
+        broadcastAll(client, "remove grocery item", index);
+    });
 });
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/js/:filename', function(req, res) {
-  res.sendFile(__dirname + '/js/' + req.params.filename);
+app.get('/js/:filename', function (req, res) {
+    res.sendFile(__dirname + '/js/' + req.params.filename);
 });
 
-app.get('/css/:filename', function(req, res) {
-  res.sendFile(__dirname + '/css/' + req.params.filename);
+app.get('/css/:filename', function (req, res) {
+    res.sendFile(__dirname + '/css/' + req.params.filename);
 });
 
 server.listen(process.env.PORT || 8080);
